35d03785f113812530d638f9a7af2129e812ba1c
Vaibhav Alur <vsalur22@gmail.com>
2021-05-04 18:31:53 +0530

    Added HTML CSS code to Flask App. MQTT Not Done yet.

--- webApp/app.py
+++ webApp/app.py
@@ -1,8 +1,12 @@
 # import libraries
 from flask import Flask, render_template
+import flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
